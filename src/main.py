6914ea3e70e315b0ee8d8705e9043542bd20fdbe
Anmol Tuteja <tutejaanmol54@gmail.com>
2026-02-22 19:21:11 +0530

    feat(rag) : add  embedding manager

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.rag.github_codebase_loader import GitHubCodeBaseLoader
 from src.rag.text_splitter import TextSplitter
+from src.rag.embedding_manager import EmbeddingManager
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,3 +10,4 @@ if __name__ == "__main__" :
     docs = GitHubCodeBaseLoader(repo="AnmolTutejaGitHub/portfolio",branch="main",access_token=os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")).load()
     chunks = TextSplitter(docs).split_documents_into_chunks()
     print(chunks)
+    embedding_manager = EmbeddingManager()
